190efe0b62469f79ccfa338f7292c66036c1c57b
csvinhal <cristiano.vinhal10@gmail.com>
2020-02-17 20:16:16 -0300

    Updates

---
+++ precache-manifest.09281aaf24dbbe684280b13f155382ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772f175fc1bc770fec808b68405fefea",
+    "url": "/pokemon-encyclopedia/index.html"
+  },
+  {
+    "revision": "90b8aa370feeb25554c6",
+    "url": "/pokemon-encyclopedia/static/css/main.43411f93.chunk.css"
+  },
+  {
+    "revision": "bb45e3de83fabd36c7fb",
+    "url": "/pokemon-encyclopedia/static/js/2.3f5deb89.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pokemon-encyclopedia/static/js/2.3f5deb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b8aa370feeb25554c6",
+    "url": "/pokemon-encyclopedia/static/js/main.2a748193.chunk.js"
+  },
+  {
+    "revision": "f2b0999b293cae0f1d4a",
+    "url": "/pokemon-encyclopedia/static/js/runtime-main.8fbcaa51.js"
+  },
+  {
+    "revision": "284cf94b44b6663348feb7ed7af91e95",
+    "url": "/pokemon-encyclopedia/static/media/DIN-Alternate-Medium.284cf94b.otf"
+  },
+  {
+    "revision": "d3085f686df272f9e1a267cc69b2d24f",
+    "url": "/pokemon-encyclopedia/static/media/Montserrat-Bold.d3085f68.ttf"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/pokemon-encyclopedia/static/media/Montserrat-Regular.07689d4e.ttf"
+  },
+  {
+    "revision": "816e4ead403eeba090d9a7c77c4ba9c2",
+    "url": "/pokemon-encyclopedia/static/media/Montserrat-UltraLight.816e4ead.otf"
+  },
+  {
+    "revision": "fc38e0e9f1b3ad15767018762b0d4b6a",
+    "url": "/pokemon-encyclopedia/static/media/background.fc38e0e9.png"
+  }
+]);
